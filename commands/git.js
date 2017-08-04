84692e53a16693ff6cf57eae36999e51002a061b
ExModify <exmodify@gmail.com>
2017-08-04 23:59:13 +0200

    2.4 Git Embed color added

--- commands/git.js
+++ commands/git.js
@@ -8,11 +8,12 @@ module.exports = {
     requirePrefix: true,
     execute: (bot, message, prefix, command, parameter, language) => {
         var proc = exec('git log --format=%B -n 1', (error, out, err) => {
-            var lines = out.split('\n', 2);
+            var lines = out.split('\n');
             var Embed = new Discord.RichEmbed();
             Embed.setTitle("Github - " + lines[0]);
-            Embed.setDescription(lines[1]);
+            Embed.setDescription(lines.slice(1).join('\n'));
             Embed.setURL("https://github.com/ExModify/ReExMoBot.js");
+            Embed.setColor(0 << 16 | 255 << 8 | 255);
             message.channel.send({embed:Embed});
         });
     }
