d47e03c792bedd85a23c064ee8f6f531ed572f7c
ExModify <exmodify@gmail.com>
2017-08-10 18:41:00 +0200

    3.2 - Redesigned Entrace.js - Removed update and added a new exit code to Entrace.js - Created run.bat - Update returns boolean - Added avatar.js - Fixed execute.js, changed to eval - Removed channel id from exit in reload.js - Added restart which restarts the Entrace as well - Added update.js - Fixed .gitignore - Using buffered2 instead of thorught2, implemented some fixes - Debuging Update.js

--- Update.js
+++ Update.js
@@ -5,6 +5,7 @@ module.exports = {
     update: () => {
         console.log(chalk.yellow('[Updater]') + ' Searching for updates...');
         var output = execSync('git pull origin master').toString();
+        console.log(output);
         if(output.toLowerCase().indexOf('everything is up-to-date') >= 0){
             console.log(chalk.yellow('[Updater]') + ' No update found!');
             return false;
